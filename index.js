9188b5c9fd711cbd383c40b8d4c22f544565a1f4
Krischan <krischanklug@@icloud.com>
2024-02-21 15:37:35 +0100

    finished card

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { CharacterCard } from "./components/CharacterCard/CharacterCard.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +14,8 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+//FOR RENDER!!!!
+/*
+const card = CharacterCard("", "Henrik", "Dead", "human", "45");
+cardContainer.append(card); */
