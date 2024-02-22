ec195962c35e35fd27659152fface02ec8200f8c
Jana Scheidemann <jana.scheidemann@freenet.de>
2024-02-22 11:09:04 +0100

    add pagination

--- index.js
+++ index.js
@@ -20,28 +20,55 @@ const searchQuery = "";
 const card = CharacterCard("", "Henrik", "Dead", "human", "45");
 cardContainer.append(card); */
 
+let pageIndex = 1;
 async function fetchCharacters() {
-  const url = "https://rickandmortyapi.com/api/character";
+  const url = `https://rickandmortyapi.com/api/character?page=${pageIndex}`;
   const response = await fetch(url);
   const json = await response.json();
-  /* console.log(json.results); */
+  // console.log(json.results);
   return json.results;
 }
 
-const characterData = await fetchCharacters();
-/* console.log(characterData); */
+renderCards();
 
-cardContainer.innerHTML = "";
+async function renderCards() {
+  const characterData = await fetchCharacters();
+  /* console.log(characterData); */
 
-characterData.forEach((character) => {
-  const newCard = CharacterCard(
-    character.image,
-    character.name,
-    character.status,
-    character.type,
-    character.episode.length
-  );
-  console.log(newCard);
+  cardContainer.innerHTML = "";
 
-  cardContainer.append(newCard);
+  characterData.forEach((character) => {
+    const newCard = CharacterCard(
+      character.image,
+      character.name,
+      character.status,
+      character.type,
+      character.episode.length
+    );
+    console.log(newCard);
+
+    cardContainer.append(newCard);
+  });
+}
+
+prevButton.addEventListener("click", () => {
+  if (pageIndex == 1) {
+    return;
+  } else {
+    pageIndex = pageIndex - 1;
+  }
+  renderCards();
+  pagination.innerHTML = `${pageIndex} / 42`;
+  console.log(pageIndex);
+});
+
+nextButton.addEventListener("click", () => {
+  if (pageIndex == 42) {
+    return;
+  } else {
+    pageIndex = pageIndex + 1;
+  }
+  renderCards();
+  pagination.innerHTML = `${pageIndex} / 42`;
+  console.log(pageIndex);
 });
